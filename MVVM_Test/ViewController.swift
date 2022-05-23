2b6b0e12d46a505a6bdcf6714885e50672a4cb0a
jhjung <jhjung@aiblab.co.kr>
2022-05-23 13:29:53 +0900

    Initial Commit

---
+++ MVVM_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Test
+//
+//  Created by 정지훈 on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
